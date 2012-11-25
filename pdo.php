6857d19e0d5a100c2c2fae0cb0f1594d05a39221
Airship <airship2600@gmail.com>
2012-11-25 12:25:07 +0100

    pdo

---
+++ pdo.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: TIS
+ * Date: 25-11-12
+ */
+
+
+//Make PDO Object
+try {
+
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    );
+
+    $pdo = new PDO('mysql:host=db416507421.db.1and1.com;dbname=db416507421;charset=UTF-8','dbo416507421', 'tabletopapples',$options);
+} catch (PDOException $e) {
+    die('cant pdo: '. $e->getMessage());
+}
+
+//Run Query
+$statement = "SELECT * FROM `testtable1` WHERE `FirstName` = :first";
+$query = $pdo->prepare($statement);
+$query->execute(array(
+    ':first' => 'John'
+));
+$result = $query->fetchAll();
+
+//Show results
+echo '<pre>';
+print_r($result);
+echo '</pre>';
