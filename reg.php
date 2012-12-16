e3522a1c575e5489df27577f66d865f7460d1f75
Derek <zorfie123@gmail.com>
2012-12-16 11:21:40 +0000

    Added page to inform the user that their registration was a success,Registration system partially complete.

--- reg.php
+++ reg.php
@@ -6,11 +6,17 @@
  * Time: 11:42
  * To change this template use File | Settings | File Templates.
  */
+use Orbyte\Database\PDO\PDOQueryBuilder;
 
 include("factory.php");
+include("lib/orbyte/database/pdo/PDOQueryBuilder.php");
+
 echo "<h2>Register</h2> ";
-print
-    ("
+
+if (!isset($_COOKIE['status'])) {
+
+    print
+        ("
             <hr/>
             <form action = 'reg.php' method='post''>
             User name: <br> <input type='text' name='username' value='{$_POST["username"]}'><br>
@@ -21,43 +27,78 @@ print
             </form>
     ");
 
-if (isset($_POST['submit']))
-{
+    if (isset($_POST['submit'])) {
 
-    print("<font color='red'>");
+        print("<font color='red'>");
 
-    $ready = true;
-    if ($_POST['username'] == '') {
-        $ready = false;
-        print("User name missing. <br>");
+        $ready = true;
+        if ($_POST['username'] == '') {
+            $ready = false;
+            print("User name missing. <br>");
+        }
+
+        if (strlen($_POST['username']) > 15) {
+            $ready = false;
+            print("User name is to long. <br>");
+        }
+
+        if ($_POST['password'] == '') {
+            $ready = false;
+            print("Password missing. <br>");
+        }
+
+        if ($_POST['password'] != $_POST['password2']) {
+            $ready = false;
+            $_POST['password'] == null;
+            $_POST['password2'] == null;
+            print("Passwords do not match. <br>");
+        }
+
+        if ($_POST['email'] == '') {
+            $ready = false;
+            print("Email missing. <br>");
+        }
+
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $ready = false;
+            print("Email invalid. <br>");
+        }
+
+
+        if ($ready) {
+            $pdo = Factory::getPdo();
+            $statement = PDOQueryBuilder::insert("userdb", array("username" => $_POST['username'], "email" => $_POST['email'], "password" => $_POST['password']));
+            print($statement);
+            Factory::query($pdo, $statement);
+
+
+            print('
+                    <script type="text/javascript">
+                        window.location = "http://www.google.com/"
+                    </script>
+                ');
+            die();
+        }
     }
-
-    if ($POST['password'] == '') {
-        $ready = false;
-        print("Password missing. <br>");
-    }
-
-    if ($POST['password'] != $_POST['password2']) {
-        $ready = false;
-        print("Passwords do not match. <br>");
-    }
-
-    if ($POST['email'] == '') {
-        $ready = false;
-        print("Email missing. <br>");
-    }
-
-    if($ready)
-    {
-        //Submit information to database here
-    }
-}
     $pdo = Factory::getPdo();
-    $query = Factory::query($pdo, "SELECT * FROM `testtable1`");
+    $query = Factory::query($pdo, "SELECT * FROM `userdb`");
     echo '<pre>';
     print_r($query);
     echo '<pre>';
-    //print(Factory::query(Factory::getPdo(), "SELECT * FROM `testtable1` WHERE `FirstName` = `John`"));
+
+} else {
+    $_COOKIE['status'] == null;
+    print("Registration success </br>");
+    echo '<pre>';
+    print("Username: " . $_POST['username']);
+    print("Email: " . $_POST['email']);
+    echo '</pre>';
+    $_POST['username'] == null;
+    $_POST['email'] == null;
+    $_POST['password'] == null;
+    $_POST['password2'] == null;
+}
+
 ?>
 
 
---
+++ reg_complete.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Registration Completed</title>
+</head>
+<body>
+<h3>Registration Completed</h3> <br/>
+You may now log in using the information you entered.
+</body>
+</html>
